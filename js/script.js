93efbda6bdd49e1728736ed954258754de310546
Mark-a-Hamilton <154675184+Mark-a-Hamilton@users.noreply.github.com>
2024-01-28 10:10:18 +0000

    Initial Commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// My Javascript Library
+
+function SwitchMode() {    // Function to switch between "light" and "dark" mode
+    if (document.documentElement.getAttribute("data-bs-theme") == "dark") {
+        document.documentElement.setAttribute("data-bs-theme", "light");
+    } else {
+        document.documentElement.setAttribute("data-bs-theme", "dark");
+    }
+};
