92aa130e5c43d337307afbb1bb9f76991ac13044
Mark-a-Hamilton <154675184+Mark-a-Hamilton@users.noreply.github.com>
2025-07-21 20:03:43 +0100

    21/07/2025 2003

--- js/script.js
+++ js/script.js
@@ -7,3 +7,12 @@ function SwitchMode() {    // Function to switch between "light" and "dark" mode
         document.documentElement.setAttribute("data-bs-theme", "dark");
     }
 };
+
+function showBadgeModal(title, imagePath) {
+    document.getElementById('badgeModalLabel').textContent = title;
+    const image = document.getElementById('badgeModalImage');
+    image.src = imagePath;
+    image.alt = `${title} Badge Full View`;
+    const badgeModal = new bootstrap.Modal(document.getElementById('badgeModal'));
+    badgeModal.show();
+};
